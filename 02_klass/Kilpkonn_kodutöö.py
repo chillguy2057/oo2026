9706634e838e627f677fc0dd62021b164b6f5b8c
Artjom Kudrjašov <artkud@tlu.ee>
2026-05-01 13:21:47 +0300

    Refactor Kilpkonn class initialization and methods
    Fix constructor and movement logic for Kilpkonn class.

--- 02_klass/Kilpkonn_kodutöö.py
+++ 02_klass/Kilpkonn_kodutöö.py
@@ -1,16 +1,36 @@
 class Kilpkonn: 
-  def init(self)
+  def __init__(self)
+  self.x = x
+  self.y = y
   self.suund = "paremale"
   
  def keeraParemale(self)
-  if self.suund == "paremale"
+  if self.suund == "paremale":
     self.suund = "alla"
-elif self.suund == "alla"
+elif self.suund == "alla":
  self.suund = "vasakule"
-elif self.suund == "vasakule"
+elif self.suund == "vasakule":
  self.suund = "yles"
-elif self.suund == "yles"
+elif self.suund == "yles":
 self.suund = "paremale"
 
 def edasi(self) 
- if self.suund == ""
+ if self.suund == "paremale":
+  self.x  += 1
+elif self.suund = "yles":
+ self.y += 1
+elif self.suund = "vasakule"
+ self.x -= 1
+elif self.suund = "alla"
+ self.y -= 1
+
+k1 = kilpkonn(0;0)
+k2 = kilpkonn(10;10)
+
+k1.edasi()
+k2.keeraParemale()
+k1.edasi()
+
+print("Esimene kilpkonn:", k1.kysi_koordinaadid())
+print("Teine kilpkonn:", k2.kysi_koordinaadid())
+
