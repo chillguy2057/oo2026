153ad85eca8b486fe50ffc82cc108ebf51f53ca9
Artjom Kudrjašov <artkud@tlu.ee>
2026-05-01 12:31:12 +0300

    Kilpkonn_koduoo.py

---
+++ 02_klass/Kilpkonn_kodutöö.py
@@ -0,0 +1,16 @@
+class Kilpkonn: 
+  def init(self)
+  self.suund = "paremale"
+  
+ def keeraParemale(self)
+  if self.suund == "paremale"
+    self.suund = "alla"
+elif self.suund == "alla"
+ self.suund = "vasakule"
+elif self.suund == "vasakule"
+ self.suund = "yles"
+elif self.suund == "yles"
+self.suund = "paremale"
+
+def edasi(self) 
+ if self.suund == ""
